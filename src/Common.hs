4054a141d1c00d61d8862bb5929ab6789d020619
Adam Van Prooyen <adam.vanpr@gmail.com>
2017-12-16 17:47:38 -0500

    Add Common.hs containing types shared accross IR levels

---
+++ src/Common.hs
@@ -0,0 +1,27 @@
+module Common
+    ( Addr
+    , SizeT
+    , RegMode(..)
+    , Width(..)
+    ) where
+
+import Data.Word
+
+type Addr = Word64
+type SizeT = Word64
+
+data RegMode =
+    L -- low 8
+  | H -- high 8
+  | X -- low 16
+  | E -- low 32
+  | R -- full 64
+  deriving (Eq, Show)
+
+data Width =
+    Byte 
+  | Word -- 16 bit
+  | Dword -- 32 bit
+  | Qword -- 64 bit
+  | Oword -- 128 bit
+  deriving (Eq, Show)
