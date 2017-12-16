5a57b5e71b98fdaeb34f105f538f7bbddca96f97
Adam Van Prooyen <adam.vanpr@gmail.com>
2017-12-16 17:54:01 -0500

    Update Lib to use Cfg.construct and show output

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,34 +2,16 @@ module Lib
     ( disass
     ) where
 
-import Data.Word
-import Numeric (showHex)
-import Data.ByteString (ByteString)
+import Data.Maybe (fromMaybe)
+import Data.ByteString.Char8 (pack)
 
-import Hapstone.Capstone
-import Hapstone.Internal.Capstone as Capstone
+import ReadElf (readElf, symbol2addr)
+import X86.Cfg (construct)
 
-import ReadElf (readElf)
-
--- use example from Capstone: http://www.capstone-engine.org/lang_python.html
-intel_asm_buf = [0x90, 0x48, 0x8b, 0x05, 0xb8, 0x13, 0x00, 0x00] :: [Word8]
-
-mkDisasm buf addr = Disassembler { 
-    arch = Capstone.CsArchX86 -- CsArchArm, CsArchArm64, CsArchMips, CsArchX86, CsArchPpc, CsArchSparc, CsArchSysz, CsArchXcore
-    , modes = [Capstone.CsMode64] -- CsModeLittleEndian, CsModeArm, CsMode16 (16-bit x86), CsMode32 (32-bit x86), CsMode64 (64-bit x86-64/amd64 or PPC), CsModeThumb, CsModeMclass, CsModeV8 (ARMv8 A32), CsModeMicro, CsModeMips3, CsModeMips32r6, CsModeMipsGp64, CsModeV9 (SparcV9 mode), CsModeBigEndian, CsModeMips32, CsModeMips64
-    , buffer = buf -- [Word8]
-    , addr = addr -- Word64
-    , num = 1 -- number of instructions
-    , Hapstone.Capstone.detail = True
-    , skip = Nothing
-    , action = defaultAction
-    }
-
-disasmInstr elf addr = 0
-
--- disass :: ByteString -> IO (Either CsErr [(CsInsn, ())])
 disass :: String -> IO ()
 disass filename = do
     elf <- readElf filename
-    res <- disasmIO (mkDisasm intel_asm_buf 0x1000)
+    let addr = fromMaybe 0x63a (symbol2addr elf (pack "main"))
+    let cfg = construct elf addr
+    putStrLn $ show cfg
     return ()
