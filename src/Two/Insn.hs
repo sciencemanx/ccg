52c0a7646cf648c5e0ac7e1f217c74fd8b15d39f
Adam Van Prooyen <adam.vanpr@gmail.com>
2017-12-18 05:08:02 -0500

    Update Two Insn

--- src/Two/Insn.hs
+++ src/Two/Insn.hs
@@ -5,11 +5,13 @@ module Two.Insn
     , MemStruct(..)
     , Cond(..)
     , CondOp(..)
+    , CondExp(..)
     , CmpOp(..)
     ) where
 
 import Data.List (intercalate)
 import Data.Word
+import Data.Int
 import Data.Maybe (maybeToList)
 
 import Common
@@ -19,6 +21,7 @@ data CondOp =
     | Minus -- sub, cmp, dec (maybe)
     | And' -- and, test
     | Or' -- or
+    | Xor' -- xor
     -- | Neg dont want to do this now
     -- | Shift{R/L, U/A} dont want to do this now
     deriving (Eq)
@@ -38,13 +41,15 @@ data CmpOp =
     | Be
     deriving (Eq)
 
+type CondExp = (Op, CondOp, Op)
+
 -- l {+,-,&,|} r {>,<,=...} 0
-data Cond = Cond Op CondOp Op CmpOp deriving (Eq)
+data Cond = Cond CondExp CmpOp deriving (Eq)
 
 data MemStruct = MemStruct
     { base :: Maybe BaseReg
-    , index :: Maybe (BaseReg, Int) -- reg * scale
-    , disp :: SizeT }
+    , index :: Maybe (BaseReg, Int32) -- reg * scale
+    , disp :: Int64 }
     deriving (Eq)
 
 data BaseReg = BaseReg Int deriving (Eq)
@@ -58,7 +63,7 @@ data Op =
 data Insn = 
       Add Op Op -- d += s
     | And Op Op -- d &= s
-    | Call Op Op -- d = s() todo: decide to infer calling conv
+    | Call Op -- s()
     | Cast Op Op Width -- d = (w) s ... aka CBW/CWDE/CDQE
     | Conv Op Op Op Width -- d:d' = (w) s ... aka CWD/CDQ/CQO
     | SDiv Op Op Op -- q' = q:q' / s; q = q:q' % s
@@ -72,18 +77,25 @@ data Insn =
     | Neg Op Op -- d = -s
     | Not Op Op -- d = ~s
     | Or Op Op -- d |= s
-    | Pop Op -- IDK might elab
-    | Push Op -- IDK might elab
     | AShiftL Op Op -- d <<= s
     | AShiftR Op Op -- d >>= s
     | ShiftL Op Op -- d <<= s
     | ShiftR Op Op -- d >>= s
     | Sub Op Op -- d -= s
-    | Test
-    -- Cmp l r: elab away
-    -- Dec s: elab to s -= 1
-    -- Inc: elab to s -= 1
+    | Xor Op Op -- d ^= s
+    | Ret
+    -- TODO:
+    -- Switch Op (Map opval Addr)
+    -- Mod
+    -- Rem
+    -- NOTES:
+    -- Dec: elab to op -= 1
+    -- Inc: elab to op += 1
+    -- Pop: elab to op = *esp; esp += sizeof(op)
+    -- Push: elab to esp -= sizeof(op); *esp = op
+    -- Cmp: elab away
     -- Nop: elab away
+    -- Test: elab away
     deriving (Eq, Show)
 
 instance Show CondOp where
@@ -91,6 +103,7 @@ instance Show CondOp where
     show Minus = "-"
     show And' = "&"
     show Or' = "|"
+    show Xor' = "^"
 
 instance Show CmpOp where
     show Eq = "=="
@@ -107,7 +120,7 @@ instance Show CmpOp where
 join = intercalate
 
 instance Show Cond where
-    show (Cond l op r cmpop) = (join " " toks) ++ " 0"
+    show (Cond (l, op, r) cmpop) = (join " " toks) ++ " 0"
       where
         toks = [show l, show op, show r, show cmpop]
 
@@ -124,4 +137,5 @@ instance Show BaseReg where
 
 instance Show Op where
     show (Reg baseReg mode) = show baseReg ++ "." ++ show mode
-    show x = show x
+    show (Imm n) = show n
+    show (Mem m) = show m
